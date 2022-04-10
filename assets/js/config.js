5594ce15830599ca4bb688d02f94d4878ed072ff
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-04-10 20:34:07 +0500

    origin

---
+++ assets/js/config.js
@@ -0,0 +1,12 @@
+  tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            gold: '#b89535',
+            light:'#f9f7f2',
+            offWhite:'#f3efe5',
+            black:'#13100d'
+          }
+        }
+      }
+    }
