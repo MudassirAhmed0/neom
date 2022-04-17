0e2a9e9364f3f7a8bb32a413a8d22387d868cccd
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-04-17 05:18:15 +0500

    origin

--- assets/js/division.js
+++ assets/js/division.js
@@ -7,3 +7,12 @@ const openTab=(id,index)=>{
     tab.classList.add('active')
     tabButtons[index].classList.add('active')
 }
+const openTab2=(id,index)=>{
+    const tab = document.getElementById(id) 
+    const tabs = document.querySelectorAll('.tab2')
+    const tabButtons = document.querySelectorAll('.tabsHeaderButton2') 
+    tabs.forEach(item=>item.classList.remove('active'))
+    tabButtons.forEach(item=>item.classList.remove('active'))
+    tab.classList.add('active')
+    tabButtons[index].classList.add('active')
+}
