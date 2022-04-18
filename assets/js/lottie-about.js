b50e9374f28d249c88579979fa5f78fc2efe66dc
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-04-18 19:32:31 +0500

    origin

---
+++ assets/js/lottie-about.js
@@ -0,0 +1,20 @@
+LottieInteractivity.create({
+    player:'#firstLottie',
+    mode:"scroll",
+    actions: [
+        {
+        visibility: [0.50, 1.0],
+        type: "play"
+        }
+    ]
+})
+LottieInteractivity.create({
+    player:'#secondLottie',
+    mode:"scroll",
+    actions: [
+        {
+        visibility: [0.50, 1.0],
+        type: "play"
+        }
+    ]
+})
---
+++ assets/js/lottie-home.js
@@ -0,0 +1,10 @@
+LottieInteractivity.create({
+    player:'#firstLottie',
+    mode:"scroll",
+    actions: [
+        {
+        visibility: [0.50, 1.0],
+        type: "play"
+        }
+    ]
+});
