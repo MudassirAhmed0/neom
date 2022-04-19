cc230ca5a36067c63b3cb5fb5de3116d63ab9ac6
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-04-19 18:30:53 +0500

    origin

---
+++ assets/js/lottie-strategy.js
@@ -0,0 +1,63 @@
+LottieInteractivity.create({
+    player:'.lottilines1',
+    mode:"scroll",
+     container: "#benchmarking_tab_and_content",
+    actions: [
+        {
+        visibility: [0.50, 1.0],
+        type: "play"
+        }
+    ]
+});
+LottieInteractivity.create({
+    player:'.lottilines2',
+    mode:"scroll",
+     container: "#benchmarking_tab_and_content",
+    actions: [
+        {
+        visibility: [0.50, 1.0],
+        type: "play"
+        }
+    ]
+});
+LottieInteractivity.create({
+    player:'.lottilines3',
+    mode:"scroll",
+     container: "#benchmarking_tab_and_content",
+    actions: [
+        {
+        visibility: [0.50, 1.0],
+        type: "play"
+        }
+    ]
+});
+LottieInteractivity.create({
+    player:'.lottilines4',
+    mode:"scroll",
+     container: "#benchmarking_tab_and_content",
+    actions: [
+        {
+        visibility: [0.50, 1.0],
+        type: "play"
+        }
+    ]
+});
+
+
+
+
+
+
+/* let player1 = document.getElementById("lottilines1");
+let player2 = document.getElementById("lottilines2");
+let player3 = document.getElementById("lottilines3");
+let player4 = document.getElementById("lottilines4");
+let play = document.getElementById("alltabs");
+
+function myanimation () {
+    alert("working");
+    player1.play();
+    player2.play();
+    player3.play();
+    player4.play();
+}; */
