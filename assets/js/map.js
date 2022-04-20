a6ffdea7c2333676501f7ca1c398e279aff018f6
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-04-21 01:19:44 +0500

    origin

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,6 +4,8 @@ function myMap() {
         // center: { lat: 28.739279, lng: 35.25263 },
         zoom: 17.5,
         disableDefaultUI: true,
+        draggable: false, 
+        zoomControl: false,
         styles: [
             //   bg color 
             { elementType: "geometry", stylers: [{ color: "#13100d" }] },
@@ -120,11 +122,69 @@ function myMap() {
 };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
+    // const contentString =
+    // '<div id="content">' +
+    // '<div id="siteNotice">' +
+    // "</div>" +
+    // '<h1 id="firstHeading" class="firstHeading">Madinah Station</h1>' +
+    // '<div id="bodyContent">' +
+    // "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    // "sandstone rock formation in the southern part of the " +
+    // "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    // "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    // "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    // "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    // "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    // "Aboriginal people of the area. It has many springs, waterholes, " +
+    // "rock caves and ancient paintings. Uluru is listed as a World " +
+    // "Heritage Site.</p>" +
+    // '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    // "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    // "(last visited June 22, 2009).</p>" +
+    // "</div>" +
+    // "</div>";
+    const contentString =`
+    <div id="content" class='min-w-[250px]'>
+    <div id="siteNotice">
+    </div>
+    <h4 id="firstHeading" class="mt-4 firstHeading text-2xl text-center">Neom  Cybersecurity</h4>
+    <div id="bodyContent">
+    <div class="flex flex-col  items-center opacity-[70%] my-4">
+    <div class="flex border   border-[#ccc]">
+        <span class="border-[#ccc] border-r p-2 pr-4 mr-2">Sun-Thu</span>
+        <span class="p-2">(7AM - 11PM)</span>
+    </div>
+    <div class="flex border-b border-r   border-l border-[#ccc]">
+        <span class="border-[#ccc] border-r p-2 pr-4 mr-2">Sun-Thu</span>
+        <span class="p-2">(7AM - 11PM)</span>
+    </div>
+</div>
+<a href="https://www.google.com/maps/place/28%C2%B006'45.0%22N+35%C2%B006'34.4%22E/@28.1125116,35.1073741,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x135dbda222152280!8m2!3d28.1125116!4d35.1095628" target="_blank" class="mb-4 justify-center text-gold hover:opacity-[70%] transition-all duration-300 underline brown-light gap-x-2 flex items-center">
+        Get Direction
+        <img class="w-[18px]" src="./assets/images/svg/external-link.svg" alt="">
+    </a>
+    </div>
+    </div>
+    `
+
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+
+
     marker = new google.maps.Marker({
         position: new google.maps.LatLng(28.112972, 35.109409),
         map: map,
              icon: icon,
     });
 
-
+    marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: false,
+        });
+      });
+    
 }
--- assets/js/myCarousel.js
+++ assets/js/myCarousel.js
@@ -1,14 +1,14 @@
-var slideNumber =1
-const slide  =(slideNum)=>{ 
-    slideNumber = slideNum? slideNum :slideNumber
+var slideNumber = 1
+const slide = (slideNum) => {
+    slideNumber = slideNum ? slideNum : slideNumber
     const nav = document.querySelector('.slideNav')
     const balls = document.querySelectorAll('.nav-ball')
     const slides = document.querySelectorAll('.slide')
-    slides.forEach(slide=>{
+    slides.forEach(slide => {
         slide.classList.add('hidden')
         slide.classList.remove('slide-active')
     })
-    switch (slideNum? slideNum :slideNumber) {
+    switch (slideNum ? slideNum : slideNumber) {
         case 1:
             nav.classList.add('gradient1')
             nav.classList.remove('gradient2')
@@ -30,7 +30,7 @@ const slide  =(slideNum)=>{
             nav.classList.add('gradient2')
             nav.classList.remove('gradient3')
             nav.classList.remove('gradient4')
-            
+
             slides[1].classList.remove('hidden')
             slides[1].classList.add('slide-active')
 
@@ -62,7 +62,7 @@ const slide  =(slideNum)=>{
             nav.classList.remove('gradient2')
             nav.classList.remove('gradient3')
             nav.classList.add('gradient4')
-            
+
             slides[3].classList.remove('hidden')
             slides[3].classList.add('slide-active')
 
@@ -72,9 +72,9 @@ const slide  =(slideNum)=>{
             balls[2].classList.remove('bg-[#bfbdb9]')
             balls[3].classList.add('carouselcircleBlue')
             balls[3].classList.remove('bg-[#bfbdb9]')
-            
+
             break;
-     
+
     }
 }
 
@@ -84,31 +84,47 @@ const slide  =(slideNum)=>{
 let start = false
 
 
+let bool = false;
 
+const startCarousel = () => {
 
-
-const startCarousel =()=>{
-  
-        let el = document.querySelector('.myCarousel')
-        var rect = el.getBoundingClientRect();
-    
+    let el = document.querySelector('.myCarousel')
+    var rect = el.getBoundingClientRect();
+    // console.log("bool", bool);
+    if (bool !== true) {
+        // console.log("bool", bool);
         if (
             rect.top >= 0 &&
             rect.left >= 0 &&
             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
             rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
-        ){
+        ) {
             start = true
         }
-        if(start){
-            
-            setInterval(()=>{ 
-                slideNumber= slideNumber !== 4? slideNumber + 1: 1
+    }
+    if (start) {
+
+        setInterval(() => {
+            if (bool == false) {
+                slideNumber = slideNumber !== 4 ? slideNumber + 1 : 1
                 slide()
-            },6000)
-            window.removeEventListener('scroll',startCarousel)
-        }
-    
+            }
+        }, 6000)
+        window.removeEventListener('scroll', startCarousel)
+    }
+
 }
 
-window.addEventListener('scroll',startCarousel)
+window.addEventListener('scroll', startCarousel)
+
+
+const stopCarousel = () => {
+    // console.log("bool", bool);
+    bool = true;
+    // console.log("bool", bool);
+}
+const startTheCarousel = () => {
+    // console.log("bool", bool);
+    bool = false;
+    // console.log("bool", bool);
+}
